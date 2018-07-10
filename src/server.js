faef32ad683c8b5f992b37490036449407fe1eda
Shobhit Chittora <chittorashobhit@gmail.com>
2018-07-10 14:09:52 +0530

    grpc server

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const grpc = require('grpc');
+const loader = require('@grpc/proto-loader');
+const todos = require('./todos_db.json');
+
+class TodoAppHandler {
+  getAll(_, callback) {
+    return callback(null, todos);
+  }
+
+  getTodo(call, callback) {
+    const result = todos.todos.filter(({ id }) => id === call.request.id);
+    return callback(null, result[0]);
+  }
+}
+const PATH = '0.0.0.0:8080';
+
+const createServer = function (path, handler) {
+  loader.load('todo.proto', { includeDirs: ['./src'] })
+    .then((packageDefinition) => {
+      const package = grpc.loadPackageDefinition(packageDefinition);
+      const service = package.todo_app_package.TodoApp.service;
+      const server = new grpc.Server();
+      server.addService(service, handler);
+      server.bind(path, grpc.ServerCredentials.createInsecure());
+      server.start();
+      console.log('Server running on 8080');
+    });
+}
+
+createServer(PATH, new TodoAppHandler);
